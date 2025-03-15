e0f517563a14034bcdcdad98883d19dcfd52c7db
dlfhrrl12 <apfhdfhrrl@naver.com>
2025-03-15 20:20:14 +0900

    Active Link

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,37 @@
+"use client";
+import { usePathname } from "next/navigation";
+import "../globals.css";
+import Link from "next/link";
+
+const navLink = [
+  { name: "Register", href: "/register" },
+  { name: "Login", href: "/login" },
+  { name: "Forgot Password", href: "/forgot-password" },
+];
+
+export default function AuthLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  const pathname = usePathname();
+  return (
+    <div>
+      {navLink.map((link) => {
+        const isActive =
+          pathname === link.href ||
+          (pathname.startsWith(link.href) && link.href !== "/");
+        return (
+          <Link
+            className={isActive ? "font-bold mr-4" : "text-blue-500 mr-4"}
+            href={link.href}
+            key={link.name}
+          >
+            {link.name}
+          </Link>
+        );
+      })}
+      {children}
+    </div>
+  );
+}
